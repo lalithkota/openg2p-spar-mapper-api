133d67e228d3aca0f2014dbab7a0ab64ec573565
PSNAppZ <thepsnarayanan@gmail.com>
2024-03-21 14:38:42 +0530

    Refactor file structure and imports

--- openg2p-spar-mapper-api/src/app.py
+++ openg2p-spar-mapper-api/src/app.py
@@ -1,13 +1,38 @@
 # ruff: noqa: E402
+import asyncio
 
 from .config import Settings
 
 _config = Settings.get_config()
 
-from openg2p_fastapi_common.app import Initializer
+from openg2p_fastapi_common.app import Initializer as BaseInitializer
+from .controllers.g2pconnect.sync_mapper_controller import SyncMapperController
+from .models.orm.id_fa_mapping import IdFaMapping
+from .services_g2pconnect import (
+    MapperService,
+    SyncRequestHelper,
+    RequestValidation,
+    SyncResponseHelper,
+    IdFaMappingValidations,
+)
 
 
-class Initializer(Initializer):
+class Initializer(BaseInitializer):
     def initialize(self, **kwargs):
         super().initialize()
-        # Initialize all Services, Controllers, any utils here.
+
+        MapperService()
+        IdFaMappingValidations()
+        SyncRequestHelper()
+        RequestValidation()
+        SyncResponseHelper()
+        SyncMapperController().post_init()
+
+    def migrate_database(self, args):
+        super().migrate_database(args)
+
+        async def migrate():
+            print("Migrating database")
+            await IdFaMapping.create_migrate()
+
+        asyncio.run(migrate())
