0d28b859621c7dbf4aa40d4e05d1f107b1163725
vineela-afk <ampilivineela@gmail.com>
2024-03-28 11:56:49 +0530

    Added the unlink and packaging

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# ruff: noqa: I001
+
+from openg2p_spar_mapper_api.app import Initializer
+# from openg2p_fastapi_common.ping import PingInitializer
+
+main_init = Initializer()
+
+main_init.main()
