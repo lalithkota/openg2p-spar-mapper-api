ebc9e0acc9812b8b73f9e963f6da36753eaf5dc0
PSNAppZ <thepsnarayanan@gmail.com>
2024-03-28 15:13:25 +0530

    Fix main.py import path

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # ruff: noqa: I001
 
-from openg2p_spar_mapper_api.app import Initializer
+from src.openg2p_spar_mapper_api.app import Initializer
+
 # from openg2p_fastapi_common.ping import PingInitializer
 
 main_init = Initializer()
